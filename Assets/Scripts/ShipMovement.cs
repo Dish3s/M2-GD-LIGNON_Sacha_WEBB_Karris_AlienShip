1020011d959f513509abd69fab29298da99b6ab5
Dish3s <131878820+Dish3s@users.noreply.github.com>
2024-01-30 11:51:24 +0100

    [Thanks Evann]

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -1,28 +1,61 @@
 using UnityEngine;
 using System.Collections;
+using System.Runtime.ConstrainedExecution;
+using Unity.VisualScripting;
+using UnityEngine.XR;
 
 public class ShipMovement : MonoBehaviour
 {
     [Header("Movement")]
+    //Speed of movement
     [SerializeField] float speed;
+    //Sets the speed of rotation
+    [SerializeField] float angle = 2;
 
-    float rotX;
-    float rotY;
-    float rotZ;
+    private Vector2 input;
 
+    Quaternion pitchY;
+    Quaternion rollZ;
+
+    //Quaternion X = ;
+
+//CALLING INPUTS - Defined in Unity Input Manager
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.rotation = (rotX*rotY)*transform.rotation*(rotY*rotX);
-        }
+        //Inputs to get axis based on x, Vertical is not used
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * Time.deltaTime;
+
+        //Use theta to calculate the Quaternion of the rotation, the angle used her is the angle used in the Theta function
+        pitchY = theta(Vector3.up, angle * input.x );
+        rollZ = theta(Vector3.forward, angle * -input.x);
+
+        //transform.localRotation = Using rotation of the SHIP and not the world your ship can now rotate multipled by the Quaternion Rotations
+        transform.localRotation = pitchY * transform.localRotation * rollZ;
+
+        //GO FORWARD
+        transform.position += transform.forward * speed * Time.deltaTime;
 
     }
-    public Quaternion Conjugate(Quaternion q)
+
+//QUATERNION TO THETA ANGLE
+    private Quaternion theta(Vector3 axis, float angle)
     {
-        return new Quaternion(-q.x, -q.y, -q.z, q.w);
+        //Translate the angles which are in Degrees into Radians
+        angle = Mathf.Deg2Rad * angle;
+
+        //Taking the axis of rotation, and finding the Theta angle, converts it to Quaternion
+        Quaternion rotation = new Quaternion
+            
+            (
+            axis.x * Mathf.Sin(angle / 2),
+            axis.y * Mathf.Sin(angle / 2),
+            axis.z * Mathf.Sin(angle / 2),
+            Mathf.Cos(angle / 2)
+            );
+
+        return rotation;
+        //Rotates the ship
+            
     }
-
-
 
 }
